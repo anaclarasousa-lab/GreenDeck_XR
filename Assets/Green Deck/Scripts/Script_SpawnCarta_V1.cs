646bf6da08b114f34d27c768d21a3aef7a6ef740
Ana Sousa <ana.clara.sousa@estudiantat.upc.edu>
2025-11-30 13:29:38 +0100

    Spawn de cartas
    Ahora se pueden spawnear cartas y se colisionan con los planos detectados :D

---
+++ Assets/Green Deck/Scripts/Script_SpawnCarta_V1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpawnCarta : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public float spawnSpeed = 2;
+    public InputActionProperty inputAction;
+
+    //Update is called once per  frame
+    void Update()
+    {
+        if(inputAction.action.WasPressedThisFrame())
+        {
+            GameObject spawnedCube = Instantiate(cubePrefab, transform.position, transform.rotation);
+            Rigidbody cubeRigibody = spawnedCube.GetComponent<Rigidbody>();
+            cubeRigibody.velocity = transform.forward * spawnSpeed;
+        }
+    }
+}
