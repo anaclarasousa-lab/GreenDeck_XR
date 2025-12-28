c76a2b9d2b36abdf152fb7f93a4ff0640cb13e4e
Boo <164067026+BooStudiooEdu@users.noreply.github.com>
2025-12-28 11:54:04 +0100

    Starting Display to Main scene.
    Script done for starting the game in the start display and enter the main scene once the button "Start " is pressed.

---
+++ Assets/Green Deck/Scripts/Change Scenes.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+    public void GoToMainScene()
+    {
+        SceneManager.LoadScene("Escena_GreenDeckGame_V21");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+  
+}
